6733580b050628b8a3a441ecfc2e82ef8aca25ed
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-14 22:30:52 +0530

    Python.py

---
+++ If-elif-else/03.py
@@ -0,0 +1,12 @@
+# Write a program to check whether a number is divisible by 7 or not.
+
+print("Check a Number is divisible by 7  answer - Yes or not ?")
+
+num = int(input("Enter the Number : "))
+if(num%7==0):
+    print("Yes")
+else:
+    print("No")
+
+# For not close immediately 
+input("Press Enter to close program")
