4b031d19e92c908b78c4d51494279a84db0ac56f
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-14 21:57:11 +0530

    Python.py

--- If-elif-else/01.py
+++ If-elif-else/01.py
@@ -1,1 +1,8 @@
-hello
+# Write a program to check whether a person is eligible for voting or not. (accept age from user)
+
+print("Enter Your Age Eligible for Voting :- ")
+age = int(input("Enter Your age : "))
+if age>=18:
+    print("You are Eligible for Voting ")
+else:
+    print("You are not Eligible for Voting")
