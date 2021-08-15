b3a446dd8c3b8137056780b6c57d970311c38705
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 01:05:44 +0530

    Python.py

---
+++ If-Elif-Else/02 - Test/07.py
@@ -0,0 +1,13 @@
+#  Accept any city from the user and display monument of that city.
+#                   City                                 Monument
+#                   Delhi                               Red Fort
+#                   Agra                                Taj Mahal
+#                   Jaipur                              Jal Mahal
+
+
+print("Accept any city to display famous monument fo that city ")
+
+city = input("Enter the City name : ")
+if(city==Delhi):
+    print("Red Fort")
+else:
