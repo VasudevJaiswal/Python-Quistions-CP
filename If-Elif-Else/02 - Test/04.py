22590dd8f3a423ce928ef70c3de90279f0f83048
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 00:38:27 +0530

    Python.py

--- If-Elif-Else/02 - Test/04.py
+++ If-Elif-Else/02 - Test/04.py
@@ -2,3 +2,21 @@
 # 2 for Monday and so on.
 
 print("Accept a Number from 1 to 7 to display Sun-Sat")
+
+num = int(input("Enter Your number 1 to 7 : "))
+if(num==1):
+    print("Sunday")
+elif(num==2):
+    print("Monday")
+elif(num==3):
+    print("Tuesday")
+elif(num==4):
+    print("Wednesday")
+elif(num==5):
+    print("Thursday")
+elif(num==6):
+    print("Friday")
+elif(num==7):
+    print("Saturday")
+else:
+    print("Please Enter between 1 to  7 ")
