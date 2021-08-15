ed3ffd87378509ab1fad0cce01dafa236d0d962d
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-15 22:58:49 +0530

    Python.py

--- If-elif-else/05.py
+++ If-elif-else/05.py
@@ -7,3 +7,19 @@
 
 print("Calculate Your Electricity bill Just Enter Your electricity Units ")
 
+amt = 0
+num = int(input("Enter the Number of  Uses Electricity  Units :  "))
+
+if(num<=100):
+    amt = "0Rs- No charge"
+elif(num>100):
+    amt = (num-100)*5
+elif(num>=200):
+    amt = 500+(num-200)*10
+elif(num>=350):
+    amt = "total bill Rs2000"
+
+print("Amount is ",str(amt))
+
+print("Amount is ", str(amt))
+    
