e266771ce3dcf73630da2594012edf7d4ab17d24
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 00:55:01 +0530

    Python.py

---
+++ If-Elif-Else/02 - Test/06.py
@@ -0,0 +1,11 @@
+#  What do you mean by statement
+# answer - Executable lines in a program is called instruction
+
+
+# quis 2. 
+# Write the output of the following:
+
+if True:
+    print(101)
+else:
+    print(100)
