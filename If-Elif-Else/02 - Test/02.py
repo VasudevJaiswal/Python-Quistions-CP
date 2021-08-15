32fcfb9d39a208180b097cce6c68579d1c3b48e8
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-15 23:41:06 +0530

    Python.py

---
+++ If-Elif-Else/02 - Test/02.py
@@ -0,0 +1,10 @@
+# Write a program to accept the cost price of a bike and display the road tax to be paid according to the following criteria :
+    
+#         Cost price (in Rs)                                       Tax
+#         > 100000                                                  15 %
+#         > 50000 and <= 100000                          10%
+#         <= 50000                                                  5%
+
+
+print("By Cost price of a bike - calculate  road tax to be paid ")
+Cost_Price = 
