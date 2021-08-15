5ee7599e78ce071d86a2a4e9f0d3f56d57a0db48
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-15 23:19:47 +0530

    Python.py

--- If-elif-else/06.py
+++ If-elif-else/06.py
@@ -10,3 +10,7 @@ if(id%3==0):
     print("Last digit of number is divisible by 3")
 else:
     print("Last digit of number is ''Not' divisible by 3")
+
+
+# For not close immediately 
+input("Press Enter to close program")
