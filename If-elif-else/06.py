a70b00a71833145e4f93598892a9bc6187185f95
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-15 23:18:15 +0530

    Python.py

--- If-elif-else/06.py
+++ If-elif-else/06.py
@@ -1,2 +1,12 @@
 # Write a program to check whether the last digit of a number( entered by user ) is 
 # divisible by 3 or not.
+
+print("Last Digit of Number is divisible by 3 or not ")
+
+num = int(input("Enter Any Number"))
+id = num%10     # If quis  says last digit so use %10
+
+if(id%3==0):
+    print("Last digit of number is divisible by 3")
+else:
+    print("Last digit of number is ''Not' divisible by 3")
