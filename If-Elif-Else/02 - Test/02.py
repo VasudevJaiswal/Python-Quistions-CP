cfbdcd1f7dc2b2463fc3c0504734e5e7f31b9495
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-15 23:54:14 +0530

    Python.py

--- If-Elif-Else/02 - Test/02.py
+++ If-Elif-Else/02 - Test/02.py
@@ -11,4 +11,12 @@ print("By Cost price of a bike - calculate  road tax to be paid ")
 tax = 0
 Cost_Price = int(input("Enter Cost price of Bike : "))
 
-if
+if(Cost_Price>100000):
+    tax = (Cost_Price*15)/100
+elif(Cost_Price>50000 and Cost_Price<=100000):
+    tax = (Cost_Price*10)/100
+elif(Cost_Price<=50000):
+    tax = (Cost_Price*5)/100
+
+# For not close immediately 
+input("Press Enter to close program")
