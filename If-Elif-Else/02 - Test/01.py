92b49f0fde0d387fa6438f3a69fb56be0b882764
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-15 23:25:44 +0530

    Python.py

---
+++ If-Elif-Else/02 - Test/01.py
@@ -0,0 +1,9 @@
+# Write a program to accept percentage from the user and display the grade according to the following criteria:
+
+#          Marks                                    Grade
+#          > 90                                         A
+#          > 80 and <= 90                       B
+#          >= 60 and <= 80                       C
+#          below 60                                  D
+
+print("Enter your marks for grade - gradding system ")
