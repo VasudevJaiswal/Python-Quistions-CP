206338951c637f4a026c5d39ee1ea6b9ec120172
Vasudev Jaiswal <vasujaiswal00@gmail.com>
2021-08-16 00:09:58 +0530

    Python.py

--- If-Elif-Else/02 - Test/03.py
+++ If-Elif-Else/02 - Test/03.py
@@ -6,3 +6,10 @@ year = int("Enter your Year : ")
 if(year%100==0):
     if(year%400==0):
         print("Entered Year is Leep year")
+    else:
+        print("Entered Year is not Leep year")
+else:
+    if(year%4==0):
+        print("Entered Year is Leep Year")
+    else:
+        print("Entered Year is not Leep Year ")
